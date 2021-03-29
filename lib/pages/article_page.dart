c3966dad16521e2588bad76dbaa2d0d40efede21
mashi-mashi <shima.trends@gmail.com>
2021-03-30 02:13:09 +0900

    remove floating action

--- lib/pages/article_page.dart
+++ lib/pages/article_page.dart
@@ -20,83 +20,70 @@ class ArticlePage extends HookWidget {
         .first;
 
     return Scaffold(
-        appBar: AppBar(title: Text(provider.name)),
-        backgroundColor: Constants.pageBackGroundColor,
-        body: HookBuilder(builder: (context) {
-          final futuerArticle = useProvider(articleProvider);
-          final snapshot = useFuture(
-              useMemoized(() => futuerArticle.load(providerId), [providerId]),
-              initialData: null);
+      appBar: AppBar(title: Text(provider.name)),
+      backgroundColor: Constants.pageBackGroundColor,
+      body: HookBuilder(builder: (context) {
+        final futuerArticle = useProvider(articleProvider);
+        final snapshot = useFuture(
+            useMemoized(() => futuerArticle.load(providerId), [providerId]),
+            initialData: null);
 
-          return snapshot.connectionState == ConnectionState.waiting
-              ? Center(child: CircularProgressIndicator())
-              : SafeArea(
-                  child: RefreshIndicator(
-                  onRefresh: () async {
-                    await futuerArticle.load(providerId);
-                  },
-                  child: ListView.builder(
-                      shrinkWrap: true,
-                      physics: AlwaysScrollableScrollPhysics(),
-                      itemCount: futuerArticle.articles.length,
-                      itemBuilder: (context, index) {
-                        if (index < futuerArticle.articles.length) {
-                          final article = futuerArticle.articles[index];
-                          if (index == futuerArticle.articles.length - 1) {
-                            Future(() {
-                              futuerArticle.loadNext(article.createdAt);
-                            });
-                          }
-
-                          return makeCard(article, () async {
-                            await Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(builder: (context) {
-                                return WebViewPage(article: article);
-                              }),
-                            );
+        return snapshot.connectionState == ConnectionState.waiting
+            ? Center(child: CircularProgressIndicator())
+            : SafeArea(
+                child: RefreshIndicator(
+                onRefresh: () async {
+                  await futuerArticle.load(providerId);
+                },
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: AlwaysScrollableScrollPhysics(),
+                    itemCount: futuerArticle.articles.length,
+                    itemBuilder: (context, index) {
+                      if (index < futuerArticle.articles.length) {
+                        final article = futuerArticle.articles[index];
+                        if (index == futuerArticle.articles.length - 1) {
+                          Future(() {
+                            futuerArticle.loadNext(article.createdAt);
                           });
-                        } else {
-                          return Text('記事がありません');
                         }
-                      }),
-                ));
-        }),
-        floatingActionButton: HookBuilder(builder: (context) {
-          final futuerArticle = useProvider(articleProvider);
-          return Container(
-              margin: EdgeInsets.only(bottom: 25.0),
-              child: SpeedDial(
-                icon: Icons.menu_outlined,
-                backgroundColor: Colors.black54,
-                foregroundColor: Colors.white,
-                activeIcon: Icons.remove,
-                buttonSize: 56.0,
-                visible: true,
-                elevation: 8.0,
-                shape: CircleBorder(),
-                children: [
-                  SpeedDialChild(
-                    child: Icon(Icons.favorite),
-                    backgroundColor: Colors.black54,
-                    foregroundColor: Colors.white,
-                    label: 'favorite',
-                    labelStyle: TextStyle(fontSize: 18.0),
-                    onTap: () async {
-                      await futuerArticle.load(providerId);
-                    },
-                  ),
-                  SpeedDialChild(
-                    child: Icon(Icons.copy_outlined),
-                    backgroundColor: Colors.black54,
-                    foregroundColor: Colors.white,
-                    label: 'Second',
-                    labelStyle: TextStyle(fontSize: 18.0),
-                    onTap: () async {
-                      await futuerArticle.load(providerId);
-                    },
-                  ),
-                ],
+
+                        return makeCard(article, () async {
+                          await Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) {
+                              return WebViewPage(article: article);
+                            }),
+                          );
+                        });
+                      } else {
+                        return Text('記事がありません');
+                      }
+                    }),
               ));
-        }));
+      }),
+      // floatingActionButton: HookBuilder(builder: (context) {
+      //   return Container(
+      //       margin: EdgeInsets.only(bottom: 25.0),
+      //       child: SpeedDial(
+      //         icon: Icons.menu_outlined,
+      //         backgroundColor: Colors.black54,
+      //         foregroundColor: Colors.white,
+      //         activeIcon: Icons.remove,
+      //         buttonSize: 56.0,
+      //         visible: true,
+      //         elevation: 8.0,
+      //         shape: CircleBorder(),
+      //         children: [
+      //           SpeedDialChild(
+      //             child: Icon(Icons.favorite),
+      //             backgroundColor: Colors.black54,
+      //             foregroundColor: Colors.white,
+      //             labelStyle: TextStyle(fontSize: 18.0),
+      //             onTap: () async {},
+      //           ),
+      //         ],
+      //       ));
+      // })
+    );
   }
 }
